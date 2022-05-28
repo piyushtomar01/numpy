e1b95ac1147e42698e986679d8e1aee62d483345
piyushtomar01 <105902048+piyushtomar01@users.noreply.github.com>
2022-05-28 18:16:30 +0530

    Add files via upload

---
+++ check_empty_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+ 
+the_array = np.array([])
+is_empty = the_array.size == 0
+print(is_empty)
+ 
+ 
+the_array = np.array([1, 2, 3])
+is_empty = the_array.size == 0
+print(is_empty)
